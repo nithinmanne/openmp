95a0da356ed5c05ddf34b41c32d0360e162a0504
Naga Nithin Manne <nithinmanne@gmail.com>
2018-02-18 16:53:23 +0530

    Updated nbody for specifying printing

--- nbody/nbody.cpp
+++ nbody/nbody.cpp
@@ -14,57 +14,60 @@ using namespace std;
 #define Mm 1.
 #define G 1.
 #define delt .005
+#define OUT true
 
-typedef struct cord {
+typedef struct Vec {
 	double x;
 	double y;
 	double z;
-} cord;
+} Vec;
 
-inline cord operator+(const cord& p1, const cord& p2) {
+inline Vec operator+(const Vec& p1, const Vec& p2) {
 	return { p1.x + p2.x, p1.y + p2.y, p1.z + p2.z };
 }
-inline cord operator-(const cord& p1, const cord& p2) {
+inline Vec operator-(const Vec& p1, const Vec& p2) {
 	return { p1.x - p2.x, p1.y - p2.y, p1.z - p2.z };
 }
-inline cord& operator+=(cord& p1, const cord& p2) {
+inline Vec& operator+=(Vec& p1, const Vec& p2) {
 	p1.x += p2.x;
 	p1.y += p2.y;
 	p1.z += p2.z;
 	return p1;
 }
-inline cord operator*(const double& m, const cord& p) {
+inline Vec operator*(const double& m, const Vec& p) {
 	return { m * p.x, m * p.y, m * p.z };
 }
-inline cord operator*(const cord& p, const double& m) {
+inline Vec operator*(const Vec& p, const double& m) {
 	return { m * p.x, m * p.y, m * p.z };
 }
-inline ostream& operator<<(ostream& stream, const cord& p) {
-	stream << p.x << " " << p.y << " " << p.z;
+inline ostream& operator<<(ostream& stream, const Vec& p) {
+	stream << p.x << " " << p.y << " " << p.z << "\n";
 	return stream;
 }
-inline double mag(const cord& p) {
+inline double mag(const Vec& p) {
 	return sqrt(p.x*p.x + p.y*p.y + p.z*p.z);
 }
 
 
 int main() {
-	remove("trajectory.txt");
-	remove("log.txt");
 	ofstream traj, log;
-	traj.open("trajectory.txt");
-	log.open("log.txt");
-	traj << N << " " << T << " " << delt << endl;
-	log << "Running with N = " << N << endl;
-	log << "Running with T = " << T << endl;
-	log << "Running with delt = " << delt << endl;
-	log << "Number of Threads = " << omp_get_max_threads() << endl;
+	if (OUT) {
+		remove("trajectory.txt");
+		remove("log.txt");
+		traj.open("trajectory.txt");
+		log.open("log.txt");
+		traj << N << " " << T << " " << delt << endl;
+		log << "Running with N = " << N << endl;
+		log << "Running with T = " << T << endl;
+		log << "Running with delt = " << delt << endl;
+		log << "Number of Threads = " << omp_get_max_threads() << endl;
+	}
 	auto *rad = new double[N];
 	auto *mas = new double[N];
-	auto *pos = new cord[N];
-	auto *vel = new cord[N];
-	auto *accp = new cord[N];
-	auto *acc = new cord[N];
+	auto *pos = new Vec[N];
+	auto *vel = new Vec[N];
+	auto *accp = new Vec[N];
+	auto *acc = new Vec[N];
 	for (decltype(N) i = 0; i<N; i++) {
 		rad[i] = .5;
 		mas[i] = 1;
@@ -74,7 +77,8 @@ int main() {
 		acc[i] = { 0., 0., 0. };
 	}
 	auto M = (decltype(T))ceil(T / delt);
-	log << "Running " << M << " iterations" << endl;
+	if(OUT)
+		log << "Running " << M << " iterations" << endl;
 	auto main_start = omp_get_wtime();
 	for (decltype(M) i = 0; i<M; i++) {
 		auto start = omp_get_wtime();
@@ -85,7 +89,7 @@ int main() {
 			for (decltype(N) k = 0; k<j; k++)
 				if (k != j) {
 					auto d = mag(pos[k] - pos[j]);
-					acc[j] += (G*mas[k] / pow(d, 3))*(pos[k] - pos[j]);
+					acc[j] += (G*mas[k] / (d*d*d))*(pos[k] - pos[j]);
 					if (d < rad[j] + rad[k])
 #pragma omp critical
 						collisions.insert((j > k ? j : k)*N + (j > k ? k : j));
@@ -105,11 +109,16 @@ int main() {
 		for (decltype(N) j = 0; j<N; j++)
 			accp[j] = acc[j];
 
-		for (decltype(N) j = 0; j < N; j++)
-			traj << pos[j] << endl;
-		log << "Iteration " << i << " Time " << (omp_get_wtime() - start) << " Collisions " << collisions.size() << endl;
+		if (OUT) {
+			for (decltype(N) j = 0; j < N; j++)
+				traj << pos[j];
+			log << "Iteration " << i << " Time " << (omp_get_wtime() - start) << " Collisions " << collisions.size() << "\n";
+		}
 	}
-	log << "Ran " << M << " iterations in " << (omp_get_wtime() - main_start) << endl;
-	traj.close();
-	log.close();
+	if (OUT) {
+		traj << endl;
+		log << "Ran " << M << " iterations in " << (omp_get_wtime() - main_start) << endl;
+		traj.close();
+		log.close();
+	}
 }
